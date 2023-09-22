c5c8e21984e239babbd475f151b61d46136d728b
SHIVAM AGGARWAL <shivamaggarwal0005@gmail.com>
2023-09-22 20:07:32 +0530

    jslogic

--- test.js
+++ test.js
@@ -1,9 +1,14 @@
 // always
 
-//
+// step 1
 function orderstatus() {
   setTimeout(() => {
     isorderaccepted = confirm("Do you want to accept the Order");
     console.log(isorderaccepted);
   }, 1000);
 }
+
+
+
+// 
+
